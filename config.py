f9adf063030d0fd725054346ba835ca7c7fed8e6
D-ARAS <137252149+D-ARAS@users.noreply.github.com>
2024-01-13 14:41:22 +0545

    Update config.py

--- config.py
+++ config.py
@@ -10,12 +10,12 @@ def is_enabled(value, default):
     else:
         return default
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+API_ID = int(environ.get("API_ID", "1067319"))
+API_HASH = environ.get("API_HASH", "147745612b97af716648f754d6232a1e")
+BOT_TOKEN = environ.get("BOT_TOKEN", "6159878431:AAHjLaDDdPK76ePyW259-Ypcs4HRIE3kC-E")
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
-ADMINS = int(environ.get("ADMINS", ""))
-DB_URI = environ.get("DB_URI", "")
+ADMINS = int(environ.get("ADMINS", "1017109568"))
+DB_URI = environ.get("DB_URI", "https://mandrillapp.com/track/click/31165340/auth0.openai.com?p=eyJzIjoiUno2N3l6RE04Q0JQYmFibWJwLVJPNktTei1ZIiwidiI6MSwicCI6IntcInVcIjozMTE2NTM0MCxcInZcIjoxLFwidXJsXCI6XCJodHRwczpcXFwvXFxcL2F1dGgwLm9wZW5haS5jb21cXFwvdVxcXC9lbWFpbC12ZXJpZmljYXRpb24_dGlja2V0PUN0MXZNcWxUd3J6OUxFR2xRV2h1SGppVXdGbmNYMmFrI1wiLFwiaWRcIjpcIjJkNzhjZWZmNDc4NTRlZGY5Y2VmMjhmZDdmNmUxMmM2XCIsXCJ1cmxfaWRzXCI6W1wiMWM3OTUyMjNiMmQ0YmUwMjBmZDJhNTBmMmM5YzQxZjEwMThlNDU0Y1wiXX0ifQ2")
 DB_NAME = environ.get("DB_NAME", "chatgptvjbot")
 OPENAI_API = environ.get("OPENAI_API", "")
 AI = is_enabled((environ.get("AI","True")), False)
